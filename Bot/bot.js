d795c51bb38f682e872a802771784d45b3c4b3b3
Cliff <Ockiyacliffc@gmail.com>
2024-06-29 00:20:35 +0100

    link to telegraf

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,13 +1,13 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN = "6750213592:AAG5KiG1wJfHT7n-W4tbgFqMIOO75q4GXQw";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://chop-haven.vercel.app/";
 
 bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
+  ctx.reply("Welcome to Chop Haven", {
     reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+      keyboard: [[{ text: "place your order", web_app: { url: web_link } }]],
     },
   })
 );
